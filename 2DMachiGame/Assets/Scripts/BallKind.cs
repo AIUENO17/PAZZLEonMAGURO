c7ab0af2769babae036536479e3c09b23db6e1c8
AIUENO17 <lallapallooza_1120@au.com>
2021-02-28 16:15:33 +0900

    1週間で実装

---
+++ 2DMachiGame/Assets/Scripts/BallKind.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallKind : MonoBehaviour
+{
+    public enum Ballkind // ゲーム内にて利用するピースの種類
+    {
+        FireBall = 0,    // 火
+        WaterBall,       // 水
+        WindBall,      // 緑
+        LightBall,     // 光
+        DarkBall,      // 闇
+        PinkBall,    // 回復
+    }
+}
---
+++ 2DMachiGame/Assets/Scripts/Gem.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Gem : MonoBehaviour
+{
+
+	public GameObject sphere; //子オブジェクトのSphereオブジェクト
+	public string ballName = ""; //Material検索用
+                                 //検索対象のMaterial名
+
+     string[] ballMats = { "FireBall", "WaterBall", "WindBall", "LightBall", "PinkBall", "DarkBall" };
+
+	void Start()
+	{
+		CreateGem();
+	}
+
+	public void CreateGem()
+	{
+		//マテリアルの名前をランダムで選択
+		ballName = ballMats[Random.Range(0, ballMats.Length)];
+		Debug.Log(ballName);
+		//Resources > Materials > マテリアル(ランダム)を選択
+		Sprite ballMat = Resources.Load<Sprite>("Materials/" + ballName) as Sprite;
+		Debug.Log("ballMat" + ballMat);
+		//Sphereのマテリアルをランダムで選択したマテリアルに変更
+		this.GetComponent<Image>().sprite = ballMat;
+	}
+}
+
